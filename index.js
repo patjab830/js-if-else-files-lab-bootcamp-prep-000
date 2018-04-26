9fa0be9ceb669a5c35c31e103f3cd8a1d21ed695
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-26 17:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ if ( name === "Susan" ) {
   console.log("Expected the name to be Susan.");
 }
 // Don't worry about this
-module.exports = { name, height
-}
+//module.exports = { name, height
+//}
 
